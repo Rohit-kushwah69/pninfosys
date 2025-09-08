db7a63d554d2062156eb86788c80645f129b9c83
Rohit3kushwah <rk6109744@gmail.com>
2025-09-08 15:18:26 -0500

    update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -123,7 +123,7 @@ const About = () => {
             {/* Image with Video Icon */}
             <div className="relative flex-1">
               <img
-                src="/about image/Learn.png" // use your 'Learn' image path here
+                src="/About image/Learn.png" // use your 'Learn' image path here
                 alt="Learn"
                 className="w-full rounded-lg shadow-lg"
               />
