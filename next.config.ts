f364b793783c0f32d555a013209dbe9d3f8ee247
Rohit3kushwah <rk6109744@gmail.com>
2025-09-10 02:19:54 -0500

    add

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },  
+        hostname: 'res.cloudinary.com',  // existing
+      },
+      {
+        protocol: 'https',
+        hostname: 'pnserver-0euo.onrender.com', // your live backend
+      },
     ],
   },
 };
