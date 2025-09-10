e9e944d71585b7f97a4eac23033cff2ff7804314
Rohit3kushwah <rk6109744@gmail.com>
2025-09-10 03:22:23 -0500

    edit

--- redux/features/adminAuth/adminAuthApi.js
+++ redux/features/adminAuth/adminAuthApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const adminAuthApi = createApi({
     reducerPath: "adminAuthApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api/admin",
+        baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
         credentials: "include", // send cookies for authentication
     }),
     tagTypes: ["AdminAuth"],
